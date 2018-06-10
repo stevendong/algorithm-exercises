46346b93bf8c561c877a65b9f71a22707f21f725
steven_zhou1992@126.com <genius1992>
2018-06-10 13:14:09 +0800

    read mock data from local file

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { BST } from './es6/BST/BST.class.es6';
 
+const fs = require('fs');
+
 function getRandomArr(length){
   let arr = [];
   for(var i = 0; i < length; i++){
@@ -8,6 +10,17 @@ function getRandomArr(length){
   return arr;
 }
 
-let testArr = getRandomArr(100);
-console.log(testArr.toString());
+const output = fs.readFileSync('test.txt', 'utf-8');
+if(output === ''){
+  fs.writeFileSync('test.txt', getRandomArr(100), function(err){
+    if(err){
+      return console.error(err);
+    }
+  })
+}
+const arr = [...new Set(output.split(',').map(item => parseInt(item)))];
+console.log(arr.sort((a, b) => a - b).toString());
 let testBST = new BST();
+arr.forEach(item =>{testBST.insert(item)});
+console.log(testBST.getMinNode());
+console.log(testBST.getMaxNode());
